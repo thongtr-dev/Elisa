2e7ec2651d53605f3f0c12bb66e2b29b7c9fd4bb
thongtr-dev <thongtr.dev@gmail.com>
2024-07-07 01:46:28 +0700

    feat: [BE] create exam from user past exam mistakes

--- controllers/examController.js
+++ controllers/examController.js
@@ -1,7 +1,13 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import generateExam from "../utils/generateExam.js";
+import genExamFromMistakes from "../utils/generateExamFromMistakes.js";
 import Exam from "../models/examModel.js";
 
+/**
+ * @desc Create new random exam from AI
+ * @route POST /api/exams
+ * @access Private
+ */
 const createExam = asyncHandler(async (req, res) => {
   const result = await generateExam();
   if (result.error) {
@@ -63,4 +69,55 @@ const getExamById = asyncHandler(async (req, res) => {
   }
 });
 
-export { createExam, getExamById };
+/**
+ * @desc Create new exam from AI from user past exam mistakes
+ * @route POST /api/exams/mistakes
+ * @access Private
+ */
+const createExamFromMistakes = asyncHandler(async (req, res) => {
+  const result = await genExamFromMistakes(req.user._id);
+  if (result.error) {
+    res.status(502);
+    throw new Error(`Bad Gateway: Third-party API error: ${result.error}`);
+  }
+
+  if (!result.parts) {
+    res.status(500);
+    throw new Error("Server error: Missing parts in the result");
+  }
+
+  const examData = {
+    parts: {},
+    percentage: null,
+  };
+
+  result.parts.forEach((part, index) => {
+    const partContent = JSON.parse(part.candidates[0].content.parts[0].text);
+    if (partContent) {
+      const partNumber = `part${index + 1}`;
+      examData.parts[partNumber] = partContent;
+    } else {
+      res.status(500);
+      throw new Error(`Server error: Missing exam part ${index + 1}`);
+    }
+  });
+  const percentage = result.percentage.candidates[0].content.parts[0].text;
+  if (percentage) {
+    examData.percentage = JSON.parse(percentage);
+  } else {
+    res.status(500);
+    throw new Error("Server error: Missing exam percentage match");
+  }
+
+  const exam = await Exam.create(examData);
+  if (exam) {
+    res.status(201).json({
+      examId: exam._id,
+    });
+  } else {
+    res.status(500);
+    throw new Error("Server error: Exam was not created. Please try again");
+  }
+});
+
+export { createExam, getExamById, createExamFromMistakes };
--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
 
-import { createExam, getExamById } from "../controllers/examController.js";
+import {
+  createExam,
+  createExamFromMistakes,
+  getExamById,
+} from "../controllers/examController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, createExam);
+router.route("/mistakes").post(protect, createExamFromMistakes);
 router.route("/:id").get(protect, getExamById);
 
 export default router;
